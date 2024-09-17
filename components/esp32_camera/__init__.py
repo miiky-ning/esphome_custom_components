5dd82c6b7dacaf8f66df53b6e071c4dedc480ed6
Philipp <admin@n3rd3d.com>
2024-09-17 17:53:44 +0200

    Update __init__.py

--- components/esp32_camera/__init__.py
+++ components/esp32_camera/__init__.py
@@ -282,11 +282,11 @@ async def to_code(config):
 
     if CORE.using_esp_idf:
 #        cg.add_library("espressif/esp32-camera", "2.0.9")
-        cg.add_library(
-            name="esp32-camera",
-            repository="https://github.com/espressif/esp32-camera.git",
-            version="v2.0.10",
-        )
+#        cg.add_library(
+#            name="esp32-camera",
+#            repository="https://github.com/espressif/esp32-camera.git",
+#            version="v2.0.10",
+#        )
         add_idf_sdkconfig_option("CONFIG_RTCIO_SUPPORT_RTC_GPIO_DESC", True)
         add_idf_sdkconfig_option("CONFIG_SCCB_CLK_FREQ", 20000000)
     
