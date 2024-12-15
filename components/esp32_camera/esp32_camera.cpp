83336653562466c0b7677a77b816d266296a462b
Miiky <33239206+miiky-ning@users.noreply.github.com>
2024-12-16 00:22:51 +0800

    Update esp32_camera.cpp
    try to use psram

--- components/esp32_camera/esp32_camera.cpp
+++ components/esp32_camera/esp32_camera.cpp
@@ -215,7 +215,8 @@ ESP32Camera::ESP32Camera() {
   this->config_.frame_size = FRAMESIZE_VGA;  // 640x480
   this->config_.jpeg_quality = 10;
   this->config_.fb_count = 1;
-  this->config_.fb_location = CAMERA_FB_IN_DRAM;
+//  this->config_.fb_location = CAMERA_FB_IN_DRAM;
+  this->config_.fb_location = CAMERA_FB_IN_PSRAM;
 
   global_esp32_camera = this;
 }
