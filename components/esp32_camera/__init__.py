196e0b6ea8a5b89ee112aec53d53f57cffcf6ed0
Miiky <33239206+miiky-ning@users.noreply.github.com>
2024-12-16 01:04:20 +0800

    Update __init__.py
    try to use psram

--- components/esp32_camera/__init__.py
+++ components/esp32_camera/__init__.py
@@ -281,7 +281,7 @@ async def to_code(config):
     cg.add_define("USE_ESP32_CAMERA")
 
     if CORE.using_esp_idf:
-        cg.add_library("espressif/esp32-camera", "2.0.15")
+        cg.add_library("espressif/esp32-camera", "2.0.9")
         add_idf_sdkconfig_option("CONFIG_RTCIO_SUPPORT_RTC_GPIO_DESC", True)
         add_idf_sdkconfig_option("CONFIG_SCCB_CLK_FREQ", 20000000)
         add_idf_sdkconfig_option("CONFIG_ESP32_SPIRAM_SUPPORT", True)
