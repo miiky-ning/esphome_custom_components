9115e9834980a1643ad22483a31c109d01820ab7
Miiky <33239206+miiky-ning@users.noreply.github.com>
2024-12-19 02:24:37 +0800

    support Framebuffer count
    1.support Framebuffer count. default=2
    2.support PSRAM

--- components/esp32_camera/esp32_camera.cpp
+++ components/esp32_camera/esp32_camera.cpp
@@ -214,7 +214,7 @@ ESP32Camera::ESP32Camera() {
   this->config_.pixel_format = PIXFORMAT_JPEG;
   this->config_.frame_size = FRAMESIZE_VGA;  // 640x480
   this->config_.jpeg_quality = 10;
-  this->config_.fb_count = 1;
+  this->config_.fb_count = 2;
 //  this->config_.fb_location = CAMERA_FB_IN_DRAM;
   this->config_.fb_location = CAMERA_FB_IN_PSRAM;
 
